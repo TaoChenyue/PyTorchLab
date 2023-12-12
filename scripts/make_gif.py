fa43cc53f22b1d709e65bda0b2dcf1dcfae90b50
TaoChenyue <chenyue.Tao@qq.com>
2023-12-12 10:20:56 +0800

    ✨ feat(scripts): simple scripts

---
+++ scripts/make_gif.py
@@ -0,0 +1,49 @@
+﻿from pathlib import Path
+
+from PIL import Image
+
+
+def make_gif(
+    save_path: str | Path,
+    image_dir: str | Path,
+    suffix: str = "png",
+    duration: int = 100,
+    loop: int = 0,
+    start: int = 0,
+    end: int | None = None,
+    step: int = 1,
+):
+    """
+    concrate epoch images into one gif
+
+    Args:
+        save_path (str | Path): path to save gif
+        image_dir (str | Path): directory path of images
+        suffix (str, optional): image suffix. Defaults to "png".
+        duration (int, optional): duration to display each image. Defaults to 100.
+        loop (int, optional): replay times when finishing. Defaults to 0.
+        start (int, optional): start index of image list. Defaults to 0.
+        end (int | None, optional): end index of image list. Defaults to None.
+        step (int, optional): skip images with step. Defaults to 1.
+    """
+    path_list: list[Path] = list(Path(image_dir).glob(f"*.{suffix}"))
+    path_list.sort(key=lambda x: int(x.stem.split("_")[-1]))
+    image_list: list[Image.Image] = [Image.open(x) for x in path_list]
+    if end is None:
+        end = len(image_list)
+    image_list = image_list[start:end:step]
+    image_list[0].save(
+        save_path,
+        format="GIF",
+        save_all=True,
+        append_images=image_list[1:],
+        duration=duration,
+        loop=loop,
+        optimize=True,
+    )
+
+
+if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    CLI(make_gif)
