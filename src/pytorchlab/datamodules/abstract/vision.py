5772052f31153c3a6108be06ca7aff6893642ab5
TaoChenyue <chenyue.Tao@qq.com>
2023-12-21 16:11:46 +0800

    refactor(datamodules): visiondm

--- src/pytorchlab/datamodules/abstract/vision.py
+++ src/pytorchlab/datamodules/abstract/vision.py
@@ -2,7 +2,6 @@ from abc import ABCMeta
 from typing import Callable, Iterable
 
 from torch import Tensor
-from torch.utils.data import Dataset
 from torchvision.transforms import transforms
 
 from pytorchlab.datamodules.abstract.basic import BasicDataModule
@@ -14,36 +13,3 @@ class VisionDataModule(BasicDataModule, metaclass=ABCMeta):
 
     def default_target_transforms(self) -> Callable[[Iterable], Tensor] | None:
         return None
-
-
-class TorchVisionDataModule(VisionDataModule, metaclass=ABCMeta):
-    dataset_cls: Callable[[Iterable], Dataset]
-
-    def prepare_data(self) -> None:
-        """Saves files to root."""
-        self.dataset_cls(self.train_root, train=True, download=True)
-        self.dataset_cls(self.test_root, train=False, download=True)
-
-    def entire_train_dataset(
-        self,
-        transforms: Callable[[Iterable], Tensor],
-        target_transforms: Callable[[Iterable], Tensor] | None,
-    ) -> Dataset:
-        return self.dataset_cls(
-            self.train_root,
-            train=True,
-            transform=transforms,
-            target_transform=target_transforms,
-        )
-
-    def entire_test_dataset(
-        self,
-        transforms: Callable[[Iterable], Tensor],
-        target_transforms: Callable[[Iterable], Tensor] | None,
-    ) -> Dataset:
-        return self.dataset_cls(
-            self.test_root,
-            train=False,
-            transform=transforms,
-            target_transform=target_transforms,
-        )
--- src/pytorchlab/datamodules/pytorch.py
+++ src/pytorchlab/datamodules/pytorch.py
@@ -1,11 +1,10 @@
 from typing import Callable, Iterable
 
 from torch import Tensor
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, ConcatDataset
 from torchvision import datasets
 
-from pytorchlab.datamodules.abstract.vision import (TorchVisionDataModule,
-                                                    VisionDataModule)
+from pytorchlab.datamodules.abstract.vision import VisionDataModule
 
 
 class ImageFolderDataModule(VisionDataModule):
@@ -32,17 +31,169 @@ class ImageFolderDataModule(VisionDataModule):
         )
 
 
-class MNISTDataModule(TorchVisionDataModule):
-    dataset_cls = datasets.MNIST
+class MNISTDataModule(VisionDataModule):
+    def prepare_data(self) -> None:
+        datasets.MNIST(self.train_root, train=True, download=True)
+        datasets.MNIST(self.test_root, train=False, download=True)
+
+    def entire_train_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.MNIST(
+            self.train_root,
+            train=True,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
+
+    def entire_test_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.MNIST(
+            self.test_root,
+            train=False,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
 
 
-class FashionMNISTDataModule(TorchVisionDataModule):
-    dataset_cls = datasets.FashionMNIST
+class FashionMNISTDataModule(VisionDataModule):
+    def prepare_data(self) -> None:
+        datasets.FashionMNIST(self.train_root, train=True, download=True)
+        datasets.FashionMNIST(self.test_root, train=False, download=True)
+
+    def entire_train_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.FashionMNIST(
+            self.train_root,
+            train=True,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
+
+    def entire_test_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.FashionMNIST(
+            self.test_root,
+            train=False,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
 
 
-class CIFAR10DataModule(TorchVisionDataModule):
-    dataset_cls = datasets.CIFAR10
+class CIFAR10DataModule(VisionDataModule):
+    def prepare_data(self) -> None:
+        datasets.CIFAR10(self.train_root, train=True, download=True)
+        datasets.CIFAR10(self.test_root, train=False, download=True)
+
+    def entire_train_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.CIFAR10(
+            self.train_root,
+            train=True,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
+
+    def entire_test_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.CIFAR10(
+            self.test_root,
+            train=False,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
 
 
-class CIFAR100DataModule(TorchVisionDataModule):
-    dataset_cls = datasets.CIFAR100
+class CIFAR100DataModule(VisionDataModule):
+    def prepare_data(self) -> None:
+        datasets.CIFAR100(self.train_root, train=True, download=True)
+        datasets.CIFAR100(self.test_root, train=False, download=True)
+
+    def entire_train_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.CIFAR100(
+            self.train_root,
+            train=True,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
+
+    def entire_test_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.CIFAR100(
+            self.test_root,
+            train=False,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
+
+
+class CelebADataModule(VisionDataModule):
+    def __init__(self, target_type: list[str] | str = "attr", **kwargs):
+        super().__init__(**kwargs)
+        self.target_type = target_type
+
+    def prepare_data(self) -> None:
+        datasets.CelebA(
+            self.train_root, split="all", target_type=self.target_type, download=True
+        )
+
+    def entire_train_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return ConcatDataset(
+            [
+                datasets.CelebA(
+                    self.train_root,
+                    split="train",
+                    target_type=self.target_type,
+                    transform=transforms,
+                    target_transform=target_transforms,
+                ),
+                datasets.CelebA(
+                    self.train_root,
+                    split="valid",
+                    target_type=self.target_type,
+                    transform=transforms,
+                    target_transform=target_transforms,
+                ),
+            ]
+        )
+
+    def entire_test_dataset(
+        self,
+        transforms: Callable[[Iterable], Tensor],
+        target_transforms: Callable[[Iterable], Tensor] | None,
+    ) -> Dataset:
+        return datasets.CelebA(
+            self.test_root,
+            split="test",
+            target_type=self.target_type,
+            transform=transforms,
+            target_transform=target_transforms,
+        )
