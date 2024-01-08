433cc1d4e7913c30683567ead0f83306356287e5
TaoChenyue <chenyue.Tao@qq.com>
2024-01-08 22:04:54 +0800

    fix(criterions): weighted loss

--- src/pytorchlab/criterions/weight.py
+++ src/pytorchlab/criterions/weight.py
@@ -1,4 +1,3 @@
-import torch
 from torch import nn
 
 
@@ -7,18 +6,10 @@ class WeightedLoss(nn.Module):
         self,
         criterion: nn.Module,
         weight: float = 1.0,
-        name: str = "weighted_loss",
-        choice: list[int] | None = None,
     ):
         super().__init__()
-        self.name = name
         self.weight = weight
         self.criterion = criterion
-        self.choice = choice
 
-    def forward(self, tensor_list: list[torch.Tensor], **kwargs):
-        if self.choice is None:
-            tmp_list = tensor_list
-        else:
-            tmp_list = [tensor_list[i] for i in self.choice]
-        return self.weight * self.criterion(tmp_list, **kwargs)
+    def forward(self, *args, **kwargs):
+        return self.weight * self.criterion(*args, **kwargs)
