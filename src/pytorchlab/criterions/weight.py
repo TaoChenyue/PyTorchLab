738a1b4c474312e388245fcfc29309fbdafce5e2
TaoChenyue <chenyue.Tao@qq.com>
2024-01-09 17:49:20 +0800

    feat(criterions): add adaptive loss

--- src/pytorchlab/criterions/weight.py
+++ src/pytorchlab/criterions/weight.py
@@ -1,3 +1,4 @@
+import torch
 from torch import nn
 
 
@@ -13,3 +14,19 @@ class WeightedLoss(nn.Module):
 
     def forward(self, *args, **kwargs):
         return self.weight * self.criterion(*args, **kwargs)
+
+
+def adaptive_weight_loss(criterions: list[torch.Tensor]) -> torch.Tensor:
+    """
+    Adaptive weight loss function.
+
+    Args:
+        criterions (list[torch.Tensor]): list of losses to be weighted.
+
+    Returns:
+        torch.Tensor: weighted loss.
+    """
+    total_loss = sum([crit.item() for crit in criterions])
+    weights = [crit.item() / total_loss for crit in criterions]
+    weighted_loss = sum([weight * loss for weight, loss in zip(weights, criterions)])
+    return weighted_loss
