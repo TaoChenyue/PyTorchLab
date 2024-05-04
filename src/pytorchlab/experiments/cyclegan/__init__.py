1d67a1bf8081ed4e34e882bf24f20a220459e310
TaoChenyue <chenyue.Tao@qq.com>
2024-05-04 16:24:58 +0800

    refactor(cyclegan): tiny modification
    1. lr_scheduler 2. experiment config 3. format

--- src/pytorchlab/experiments/cyclegan/__init__.py
+++ src/pytorchlab/experiments/cyclegan/__init__.py
@@ -1,3 +1,2 @@
 from .lightning_model import *
-from .lr_schedulers import *
 from .torch_model import *
--- src/pytorchlab/utils/cli.py
+++ src/pytorchlab/utils/cli.py
@@ -1,4 +1,5 @@
 from lightning.pytorch.cli import LightningCLI
 
+
 def cli():
     LightningCLI(parser_kwargs={"parser_mode": "omegaconf"})
